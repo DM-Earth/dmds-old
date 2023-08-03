5fed899efe122c4210e9431365b59db146da4bc9
Yjn024 <jiening.yu@outlook.com>
2023-08-03 10:35:48 +0800

    based

---
+++ src/collection.rs
@@ -0,0 +1,5 @@
+use crate::data::Data;
+
+pub struct Collection<T: Data> {}
+
+pub struct Chunk<T: Data> {}
--- src/data.rs
+++ src/data.rs
@@ -1,6 +1,6 @@
 use std::{
     io::{self, prelude::*},
-    ops::Add,
+    ops::Sub,
 };
 
 /// Describes a type that can be encoded bytes.
@@ -22,15 +22,65 @@ pub trait Data: Encode + Decode {
     type Identifier: Encode + Decode;
 }
 
-pub trait RangedSeek: Ord {
-    fn min() -> Self;
-    fn max() -> Self;
+pub trait RangedSeek: Sub<Output = Self::Raw> + Sized + Into<Self::Raw> {
+    type Raw: RangedSeek;
+
+    const RS_MIN: Self;
+    const RS_MAX: Self;
+
+    fn rs_div_floor(self, rhs: Self::Raw) -> usize;
+
+    fn rs_div_usize_ceil(self, rhs: usize) -> Self::Raw;
+}
+
+impl RangedSeek for u64 {
+    type Raw = Self;
+
+    const RS_MIN: Self = u64::MIN;
+
+    const RS_MAX: Self = u64::MAX;
+
+    fn rs_div_floor(self, rhs: Self::Raw) -> usize {
+        (self / rhs) as usize
+    }
+
+    fn rs_div_usize_ceil(self, rhs: usize) -> Self::Raw {
+        self / rhs as u64 + if self % rhs as u64 == 0 { 0 } else { 1 }
+    }
 }
 
 pub struct RangeTable<T>
 where
-    T: RangedSeek + Add,
+    T: RangedSeek,
 {
     separation: T,
-    chunks: usize,
+    chunk_count: usize,
+}
+
+impl<T: RangedSeek<Raw = T>> RangeTable<T> {
+    pub fn from_count<U: RangedSeek<Raw = T>>(chunk_count: usize) -> Self {
+        Self {
+            separation: (U::RS_MAX - U::RS_MIN).rs_div_usize_ceil(chunk_count),
+            chunk_count,
+        }
+    }
+
+    pub fn from_separation<U: RangedSeek<Raw = T> + Copy>(separation: U) -> Self {
+        Self {
+            separation: separation.into(),
+            chunk_count: (U::RS_MAX - U::RS_MIN).rs_div_floor(separation.into()),
+        }
+    }
+}
+
+impl<T: RangedSeek + Copy> RangeTable<T> {
+    pub fn get(&self, id: T) -> Option<usize> {
+        let raw = id.rs_div_floor(self.separation.into());
+
+        if raw <= self.chunk_count {
+            Some(raw)
+        } else {
+            None
+        }
+    }
 }
